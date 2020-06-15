ca240bda4750739b6a2cabf5efe04db206d16a09
jcr <jcrascon+github@gmail.com>
2020-06-14 23:40:35 -0700

    Created all the routes and tested

--- server.js
+++ server.js
@@ -3,44 +3,59 @@
 // Application should allow users to view previously saved notes.
 // Application should allow users to delete previously saved notes.
 
+
 //Initialize express server
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
+
 
 const PORT = 3000;
 
+// //Add middleware to express to look for files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+    res.sendFile(path.join(__dirname,'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/notes.html'));
+});
 
 
-app.listen(PORT);
+//****API METHODS */
 
-// The application frontend has already been created, it's your job to build the backend and connect the two.
+app.get('/api/notes', (req, res) => {
+    // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+    res.sendFile(path.join(__dirname,'db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = Date.now();
+    // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    let noteData = fs.readFileSync('./db/db.json');
+    let notes = JSON.parse(noteData);
+    notes.push(req.body);
+    fs.writeFileSync('./db/db.json',JSON.stringify(notes));
+
+    res.sendFile(path.join(__dirname,'public/notes.html'));
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    let noteData = fs.readFileSync('./db/db.json');
+    let notes = JSON.parse(noteData);
+    const updatedNotes = notes.filter(note => parseInt(note.id) !== parseInt(req.params.id));
+    fs.writeFileSync('./db/db.json',JSON.stringify(updatedNotes));
+    res.sendFile(path.join(__dirname,'public/notes.html'));
+});
 
 
-// The following HTML routes should be created:
-
-
-// GET /notes - Should return the notes.html file.
-
-
-// GET * - Should return the index.html file
-
-
-
-
-// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-
-
-// The following API routes should be created:
-
-
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
-
-// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
-
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
